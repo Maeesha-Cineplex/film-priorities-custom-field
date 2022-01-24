2e36938260652e413f1afb5c1db362d7460d6c1d
Maeesha Rahman <maeesha.rahman@cineplex.com>
2022-01-24 12:36:49 -0500

    add basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
+import agilityAppSDK from '@agility/app-sdk'
+
+import CustomField from './Components/CustomField';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const Components = {
+		CustomField,
+	}
+
+	const appConfig = {
+		name: 'Basic App',
+		version: '1',
+		documentationLink: 'https://github.com/agility/agility-cms-apps/tree/main/example/react',
+		configValues: [
+			{ name: 'apiKey', label: 'API Key', type: 'string' }
+		],
+		appComponents: [
+			{
+				location: agilityAppSDK.types.APP_LOCATION_CUSTOM_FIELD,
+				label: 'Basic Custom Field',
+				name: 'BasicCustomField',
+				componentToRender: 'BasicCustomField'
+			},
+			{
+				location: agilityAppSDK.types.APP_LOCATION_CUSTOM_FIELD,
+				label: 'Other Custom Field',
+				name: 'OtherCustomField',
+				componentToRender: 'BasicCustomField'
+			},
+			{
+				location: agilityAppSDK.types.APP_LOCATION_FLYOUT,
+				componentToRender: 'Flyout',
+				name: 'Flyout1'
+			}
+		]
+	};
+
+	const componentRequested = agilityAppSDK.resolveAppComponent(appConfig);
+
+	if (componentRequested === 'AppConfig') {
+
+		//provide the CMS information about your app configuration
+		agilityAppSDK.setAppConfig(appConfig);
+
+	} else {
+		//determine the React component we want to render based on what the CMS has requested...
+		const ComponentToRender = Components[componentRequested];
+
+		if (ComponentToRender) {
+			return <ComponentToRender appConfig={appConfig} />;
+		} else {
+			return <h2>Warning: App must be loaded within Agility CMS.</h2>
+		}
+	}
+
+	return null;
 }
 
 export default App;
